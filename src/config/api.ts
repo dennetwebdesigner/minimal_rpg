c59aa87b596c827ffaf7802ea9d38b37a5e10253
dennetwebdesigner <dennetwebdesigner@gmail.com>
2023-03-31 16:45:43 -0300

    test env 001

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+
 import store from "@/store/store";
+
 
 const select = store.getState();
 const token = window.localStorage.getItem("token");
 const baseURL = import.meta.env.API_URL || "http://localhost:3333";
+console.log(import.meta.env.API_URL);
 export const api = axios.create({
   baseURL,
   headers: {
